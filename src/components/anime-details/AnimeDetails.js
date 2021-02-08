9fa7c8f17e5468b43fda9595afc3785f5948f216
Souleimane <soulei.dev@gmail.com>
2021-02-08 11:51:09 +0100

    project init

---
+++ src/components/anime-details/AnimeDetails.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class AnimeDetails extends Component {
+
+    render() {
+        return <div>Anime Details</div>
+    }
+}
---
+++ src/components/anime-list/AnimeList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class AnimeList extends Component {
+
+    render() {
+        return <div>Anime List</div>
+    }
+}
