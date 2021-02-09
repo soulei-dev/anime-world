f874842731acdcf7a2409c8c658c507b489e8979
Souleimane <soulei.dev@gmail.com>
2021-02-09 11:40:07 +0100

    setting up AnimeElement component

--- src/components/anime-list/AnimeElement.js
+++ src/components/anime-list/AnimeElement.js
@@ -3,6 +3,16 @@ import React, { Component } from 'react';
 export default class AnimeElement extends Component {
 
     render() {
-        return <div>Anime Element</div>
+        return (
+            <div className='w-25 p-2'>
+                <div className='card'>
+                    <img src='https://animotaku.fr/wp-content/uploads/2020/10/Demon-slayer-kimetsu-no-yaiba-le-train-de-linfini-trailer.jpeg' className='card-img-top' alt='cover kimetsu no yaiba'/>
+                    <div className='card-body'>
+                        <h5 className='card-title'>Demon Slayer: Kimetsu no Yaiba - Le Train de l'Infini</h5>
+                        <p className='card-text'> Demon Slayer: Kimetsu no Yaiba - Le Train de l'Infini (鬼き滅めつの刃やいば 無限列車編 Kimetsu no Yaiba: Mugen Ressha Hen) est un film d'animation japonais adaptant l'Arc Train de l'Infini du manga Kimetsu no Yaiba de Koyoharu Gotôge par ufotable. Il est la suite direct de l'Adaptation en anime.</p>
+                    </div>
+                </div>
+            </div> 
+        );
     }
 }
--- src/components/anime-list/AnimeList.js
+++ src/components/anime-list/AnimeList.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
+import AnimeElement from './AnimeElement';
 
 export default class AnimeList extends Component {
 
     render() {
         return (
             <div className='w-75 d-flex flex-row flex-wrap align-content-start'>
-                <div className='w-25 p-2'>
-                    <div className='border'>Anime</div>
-                </div>
-                <div className='w-25 p-2'>
-                    <div className='border'>Anime</div>
-                </div>
-                <div className='w-25 p-2'>
-                    <div className='border'>Anime</div>
-                </div>
-                <div className='w-25 p-2'>
-                    <div className='border'>Anime</div>
-                </div>
-                <div className='w-25 p-2'>
-                    <div className='border'>Anime</div>
-                </div>
-                <div className='w-25 p-2'>
-                    <div className='border'>Anime</div>
-                </div>
+                <AnimeElement />
+                <AnimeElement />
+                <AnimeElement />
+                <AnimeElement />
+                <AnimeElement />
+                <AnimeElement />
+                <AnimeElement />
             </div>
 
         );
