7466439b8bfbe45172a93702f188269b1e06c261
Souleimane <soulei.dev@gmail.com>
2021-02-10 10:59:55 +0100

    refactoring app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import { Header, AnimeList, AnimeDetails } from './components';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      animes: [{
+        title: 'Kimetsu No Yaiba - Le Train de l\'Infini',
+        img: 'https://animotaku.fr/wp-content/uploads/2020/10/Demon-slayer-kimetsu-no-yaiba-le-train-de-linfini-trailer.jpeg',
+        details: 'A | 40 min | Japan Animation, Action',
+        description: "Demon Slayer: Kimetsu no Yaiba - Le Train de l'Infini (鬼き滅めつの刃やいば 無限列車編 Kimetsu no Yaiba: Mugen Ressha Hen) est un film d'animation japonais adaptant l'Arc Train de l'Infini du manga Kimetsu no Yaiba de Koyoharu Gotôge par ufotable. Il est la suite direct de l'Adaptation en anime."
+      }, {
+        title: 'Naruto Shippuden',
+        img: 'https://fr.web.img6.acsta.net/pictures/19/08/07/10/08/2485376.jpg',
+        details: 'A | 25 min | Japan Animation, Action',
+        description: "Cette saison revient sur l'enfance de plusieurs personnages. Plus tard, Naruto et Hinata célèbrent leur mariage, mais il y a des absents..."
+      }, {
+        title: 'Bleach',
+        img: 'https://fr.web.img6.acsta.net/pictures/19/07/19/14/52/5225691.jpg',
+        details: 'A | 25 min | Japan Animation, Action',
+        description: "Kurosaki Ichigo, un étudiant de quinze ans aux cheveux orange qui aime la bagarre (comme son père) a la particularité de voir les fantômes ainsi que de pouvoir les toucher. Cela l'amène à rencontrer Kuchiki Rukia, un Shinigami (dieu de la mort) qui combat un Hollow. Le déroulement du combat amène Kuchiki à donner ses pouvoirs à Ichigo qui deviens alors lui même un Shinigami. C'est maintenant à son tour de protéger la ville des Hollows."
+      }, {
+        title: 'Seven Deadly Sins',
+        img: 'https://www.manga-news.com/public/images/dvd/Seven-Deadly-Sins-anime-s4-visual.jpg',
+        details: 'A | 25 min | Japan Animation, Action',
+        description: "Il y a dix ans, un groupe de mercenaires appelé les Seven Deadly Sins s'est rebellé contre les Chevaliers Sacrés, la garde du royaume… Depuis, ils ont disparu et personne ne sait ce qu'ils sont devenus. Un beau jour, une mystérieuse jeune fille s'écroule dans la taverne de Meliodas, un garçon enjoué qui parcourt le monde en compagnie de son cochon loquace. Cette jeune fille n'est autre que la princesse Elizabeth qui désire ardemment retrouver les Seven Deadly Sins. En effet, ce sont les seuls à même de lutter contre les Chevaliers Sacrés, qui ont fait prisonnier le roi et qui asservissent toute la population du royaume ! Très vite, elle va découvrir que Meliodas n'est pas un simple patron de taverne mais un guerrier à la puissance exceptionnelle…"
+      }],
+      selectedAnime: 0
+    }
+  }
+
   render() {
     return (
       <div className='App d-flex flex-column'>
--- src/components/anime-list/AnimeElement.js
+++ src/components/anime-list/AnimeElement.js
@@ -4,15 +4,17 @@ export default class AnimeElement extends Component {
 
     render() {
         return (
-            <div className='w-25 p-2'>
-                <div className='card'>
-                    <img src='https://animotaku.fr/wp-content/uploads/2020/10/Demon-slayer-kimetsu-no-yaiba-le-train-de-linfini-trailer.jpeg' className='card-img-top' alt='cover kimetsu no yaiba'/>
-                    <div className='card-body'>
-                        <h5 className='card-title'>Demon Slayer: Kimetsu no Yaiba - Le Train de l'Infini</h5>
-                        <p className='card-text'> Demon Slayer: Kimetsu no Yaiba - Le Train de l'Infini (鬼き滅めつの刃やいば 無限列車編 Kimetsu no Yaiba: Mugen Ressha Hen) est un film d'animation japonais adaptant l'Arc Train de l'Infini du manga Kimetsu no Yaiba de Koyoharu Gotôge par ufotable. Il est la suite direct de l'Adaptation en anime.</p>
+            <div className='w-50 p-2'>
+                <div className='border d-flex'>
+                    <img width='150' height='200' src='https://animotaku.fr/wp-content/uploads/2020/10/Demon-slayer-kimetsu-no-yaiba-le-train-de-linfini-trailer.jpeg' alt='anime'/>
+                    <div className='flex-fill d-flex flex-column p-3'>
+                        <h5>Titre</h5>
+                        <hr className='w-100'/>
+                        <p> Demon Slayer: Kimetsu no Yaiba - Le Train de l'Infini (鬼き滅めつの刃やいば 無限列車編 Kimetsu no Yaiba: Mugen Ressha Hen) est un film d'animation japonais adaptant l'Arc Train de l'Infini du manga Kimetsu no Yaiba de Koyoharu Gotôge par ufotable. Il est la suite direct de l'Adaptation en anime.</p>
                     </div>
                 </div>
             </div> 
         );
     }
 }
+
